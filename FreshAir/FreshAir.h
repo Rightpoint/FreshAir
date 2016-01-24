3f8832a2375239a79b10142cdd257aa3d6f73dbb
Brian King <brian.king@raizlabs.com>
2016-01-23 19:02:43 -0500

    Initial Commit

---
+++ FreshAir/FreshAir.h
@@ -0,0 +1,19 @@
+//
+//  FreshAir.h
+//  FreshAir
+//
+//  Created by Brian King on 1/23/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FreshAir.
+FOUNDATION_EXPORT double FreshAirVersionNumber;
+
+//! Project version string for FreshAir.
+FOUNDATION_EXPORT const unsigned char FreshAirVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FreshAir/PublicHeader.h>
+
+
