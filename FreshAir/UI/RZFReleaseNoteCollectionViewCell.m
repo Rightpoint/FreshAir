c036a7433bfd6ec82f59099dea64409674db9434
adamhrz <adam.howitt@raizlabs.com>
2016-07-05 13:53:03 -0400

    Fixed reuse issue where images weren't being removed from the superview correctly

--- FreshAir/UI/RZFReleaseNoteCollectionViewCell.m
+++ FreshAir/UI/RZFReleaseNoteCollectionViewCell.m
@@ -78,12 +78,16 @@ static const NSInteger kRZFReleaseNoteMessageLabelFullScreenNumberOfLines = 0;
 
 - (void)prepareForReuse {
     [self.releaseNoteTitleLabel removeConstraints:self.releaseNoteTitleLabel.constraints];
-    [self.releaseNoteMessageLabel removeConstraints:self.releaseNoteMessageLabel.constraints];
-    [self.infoContainerView removeConstraints:self.infoContainerView.constraints];
-    [self.screenshotImageView removeConstraints:self.screenshotImageView.constraints];
+    [self.releaseNoteTitleLabel removeFromSuperview];
     self.releaseNoteTitleLabel = nil;
+    [self.releaseNoteMessageLabel removeConstraints:self.releaseNoteMessageLabel.constraints];
+    [self.releaseNoteMessageLabel removeFromSuperview];
     self.releaseNoteMessageLabel = nil;
+    [self.infoContainerView removeConstraints:self.infoContainerView.constraints];
+    [self.infoContainerView removeFromSuperview];
     self.infoContainerView = nil;
+    [self.screenshotImageView removeConstraints:self.screenshotImageView.constraints];
+    [self.screenshotImageView removeFromSuperview];
     self.screenshotImageView = nil;
 }
 
